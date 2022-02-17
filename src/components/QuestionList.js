e5f9578632d209e6ce6580f546a02b1adcb7b73d
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-02-17 10:22:38 -0800

    question list working

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+      setQuestions(data)
+    })
+  }, [])
+
+  const renderedQuestions = questions.map((question) => {
+    return(
+      <QuestionItem question={question}></QuestionItem>
+    )
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{renderedQuestions}</ul>
     </section>
   );
 }
