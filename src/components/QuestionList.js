5cd3859e53ecdff5b47f87f9c2f71ccbac0d1609
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-02-17 17:11:07 -0800

    assignment completed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -14,9 +14,44 @@ function QuestionList() {
     })
   }, [])
 
+  function handleDeleteClick(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE"
+    })
+    .then(r => r.json())
+    .then(() => {
+      const updatedQuestions = questions.filter((q) => q.id !== id)
+      setQuestions(updatedQuestions)
+    })
+  }
+
+  function handleAnswerChange(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({correctIndex})
+    })
+    .then((r) => r.json())
+    .then((updatedQuestion) => {
+      const updatedQuestions = questions.map((q) => {
+        if(q.id === updatedQuestion.id) return updatedQuestion
+        return q;
+      });
+      setQuestions(updatedQuestions)
+    })
+  }
+  
+
   const renderedQuestions = questions.map((question) => {
     return(
-      <QuestionItem question={question}></QuestionItem>
+      <QuestionItem 
+      question={question}
+      key={question.id}
+      handleDelete={handleDeleteClick}
+      onAnswerChange={handleAnswerChange}
+      ></QuestionItem>
     )
   })
 
